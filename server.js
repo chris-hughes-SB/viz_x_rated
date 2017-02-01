dcd919515338bdab8658d740fcc861ba58390aea
Chris Hughes <chris.hughes@simplybusiness.com>
2017-02-01 18:07:59 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, ()=>{
+  console.log('listening on 3000')
+})
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+'/index.html')
+  console.log(__dirname)
+})
+
+app.post('/price', (req,res)=>{
+  console.log(req.body)
+  res.render('index.ejs', {vals: req.body})
+})
